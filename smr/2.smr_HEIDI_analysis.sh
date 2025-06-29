b305b30101f94cfddfac3b755861ecff8a119acc
yanjingze <yjz2022njmu@163.com>
2025-06-30 00:35:18 +0800

    Add files via upload

---
+++ smr/2.smr_HEIDI_analysis.sh
@@ -0,0 +1,6 @@
+"C:/Program Files/R/R-4.3.3/library/friendlypostGWASsoft/bin/Windows/smr.exe" \
+  --bfile "../03.reference/1000Gv3/EUR"  \
+  --gwas-summary "./SMR_data/LCIS_SMR.txt"  \
+  --beqtl-summary "../03.reference/SMR/eQTLGen.lite"  \
+  --out "C:/Users/Administrator/Desktop/GWAS_POST/7.21/SMR/result/LCIS_eQTLGen"  \
+  --thread-num "16"
